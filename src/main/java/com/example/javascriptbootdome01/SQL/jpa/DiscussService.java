93c603c01cbc342c469bbda3328e7e262a7e5e8d
uuiyit <2432652710@qq.com>
2023-09-26 18:05:30 +0800

    第六章基于注解的Redis缓存实现

---
+++ src/main/java/com/example/javascriptbootdome01/SQL/jpa/DiscussService.java
@@ -0,0 +1,43 @@
+package com.example.javascriptbootdome01.SQL.jpa;//package com.example.javascriptbootdome01.SQL.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+/**
+ // * @author mrr
+ // * @date 2023/9/25 16:36
+ // * @description:
+ // */
+
+@Service//标记当前类是一个service类，加上该注解会将当前类自动注入到spring容器中，不需要再在applicationContext.xml文件定义bean了。
+public class DiscussService {
+    @Autowired
+    private DiscussRepository discussRepository;
+
+    //根据id查询评论信息
+    @Cacheable(cacheNames = "comment", unless = "#result==null")// 表示查询结果为空不进行缓存,将运行结果缓存，以后查询相同的数据，直接从缓存中取，不需要调用方法。
+    public Discuss findById(int comment_id) {
+        Optional<Discuss> optional = discussRepository.findById(comment_id);
+        //判断optional是否存在
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        return null;
+    }
+
+    @CachePut(cacheNames = "comment", key = "#result.id")//将运行结果更新
+    public Discuss updateDiscuss(Discuss discuss) {
+        discussRepository.updateDiscuss(discuss.getAuthor(), discuss.getId());
+        return discuss;
+    }
+
+    @CacheEvict(cacheNames = "comment")//将redis服务中的comment删除
+    public void deleteDiscuss(int comment_id) {
+        discussRepository.deleteById(comment_id);
+    }
+
+}
